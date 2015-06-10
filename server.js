08572828de650c3dd6fbf2d54c577d7d60b3796b
DrewMcArthur <drewmcarthur1@gmail.com>
2015-06-10 15:40:47 -0400

    server works and is set up

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-var serve = require('serve-static')('.');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var jade = require('jade');
+app.set('view engine', 'jade');
+app.locals.port = 80;
 
-var PORT = 80;
+app.use(express.static(__dirname + '/pub'));
 
-require('http').createServer(function (req, res) {
+app.get('/', function(req,res){
+	res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
 
-	serve(req, res, function (err) {
+//listen for requests at ipaddress:port specified at :19
+app.listen(app.locals.port, function(){ 
+	console.log("Server is running on port " + app.locals.port);
 
-		res.statusCode = err ? (err.statusCode || err.status) : 404;
-		res.end(err ? err.toString() : 'Cannot ' + req.method + ' ' + req.url + '\n');
+	if(process.env.USERNAME == 'root'){
+		// code snippet to change user after port is listened to for security, 
+		// found at http://syskall.com/dont-run-node-dot-js-as-root/
 
-	});
+		// Find out which user used sudo through the environment variable
+		var uid = parseInt(process.env.SUDO_UID);
+		// Set our server's uid to that user
+		if (uid) process.setuid(uid);
+		console.log('Server\'s UID is now ' + process.getuid());
+	} // end if root
 
-}).listen(PORT);
-
-if(process.env.USERNAME == 'root'){
-	// code snippet to change user after port is listened to for security, 
-	// found at http://syskall.com/dont-run-node-dot-js-as-root/
-
-	// Find out which user used sudo through the environment variable
-	var uid = parseInt(process.env.SUDO_UID);
-	// Set our server's uid to that user
-	if (uid) process.setuid(uid);
-	console.log('Server\'s UID is now ' + process.getuid());
-} // end if root
+});
